06917455495bc6e817d01018daff980024d8a36a
GIHAL <gihal83@gmail.com>
2020-06-02 22:47:52 +1200

    Enabled eureka server

--- propwand/propwand-eurekasvr/src/main/java/com/thoughtwand/propwand/eurekasvr/PropwandEurekasvrApplication.java
+++ propwand/propwand-eurekasvr/src/main/java/com/thoughtwand/propwand/eurekasvr/PropwandEurekasvrApplication.java
@@ -2,8 +2,10 @@ package com.thoughtwand.propwand.eurekasvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PropwandEurekasvrApplication {
 
     public static void main(String[] args) {
